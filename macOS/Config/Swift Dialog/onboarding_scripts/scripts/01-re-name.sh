8eea740290087bd28289aa92e6ec34323f1c98fd
ellsyeah <ellsosaurus@gmail.com>
2026-03-16 19:03:15 -0400

    Delete macOS/Config/Swift Dialog/onboarding_scripts/scripts directory

--- macOS/Config/Swift Dialog/onboarding_scripts/scripts/01-re-name.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Get serial number
-FULL_SN=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
-# Take last 6 characters
-PARTIAL_SN=${FULL_SN:(-6)}
-# Rename
-scutil --set ComputerName "GK-$PARTIAL_SN"
-scutil --set HostName "GK-$PARTIAL_SN"
-scutil --set LocalHostName "GK-$PARTIAL_SN"
